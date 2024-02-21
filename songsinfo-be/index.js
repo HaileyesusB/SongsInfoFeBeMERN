8b51e8e1dc15f59b27779d1493a87ae02db27ece
HaileyesusB <hailebelewict@gmail.com>
2024-02-21 14:18:04 +0300

    Final Update

--- songsinfo-be/index.js
+++ songsinfo-be/index.js
@@ -37,11 +37,58 @@ app.put('/updateSongs/:id', (req, res) => {
         artist: req.body.artist,
         album: req.body.album,
         gener: req.body.gener})
-    .then(song =>res.json(song))
+    .then(song =>res.json(song)) 
     .catch(err => res.json(err))
 })
 
 
+app.delete('/deleteSong/:id', (req, res) => {
+    const id = req.params.id;
+    SongsModel.findByIdAndDelete({_id:id})
+    .then(song =>res.json(song))
+    .catch(err => res.json(err))
+})
+
+  // Get songs by album
+  app.get('/songsByAlbum', async (req, res) => {
+    const albums = await SongsModel.find().populate('album'); 
+    const songsByAlbum = albums.map((album) => ({
+     songCount: album.album.length
+    }));
+    res.json(songsByAlbum.length);
+   
+  });
+
+    // Get songs by album
+    app.get('/songsByTitle', async (req, res) => {
+        const titles = await SongsModel.find().populate('title'); 
+        const songsByTitle = titles.map((title) => ({
+         songCount: title.title.length
+        }));
+        res.json(songsByTitle.length);
+       
+      });
+
+    // Get songs by album
+    app.get('/songsByArtist', async (req, res) => {
+        const artists = await SongsModel.find().populate('artist'); 
+        const songsByArtist = artists.map((artist) => ({
+         songCount: artist.artist.length
+        }));
+        res.json(songsByArtist.length);
+       
+      });
+
+      app.get('/songsByGenere', async (req, res) => {
+        const gener = await SongsModel.find().populate('gener');
+        
+        const songsByGenere = gener.map((genere) => ({
+         songCount: genere.gener
+        }));
+        console.log("ABCD" , songsByGenere)
+        res.json(songsByGenere.length);
+       
+      });
 
 app.listen(3001, () => {
     console.log("Server is running")
--- songsinfo-fe/src/SongInfo.jsx
+++ songsinfo-fe/src/SongInfo.jsx
@@ -5,24 +5,62 @@ import axios from "axios"
 
 function Songs () {
     const [songs, setSongs] = useState([])
+    const [songsByAlbum, setSongsByAlbum] = useState([]);
+    const [songsByTitle, setSongsByTitle] = useState([]);
+    const [songsByArtist, setSongsByArtist] = useState([]);
+    const [songsByGenere, setSongsByGenere] = useState([]);
+
+
     var API_URL = "http://localhost:3001"
     useEffect(() => {
-        axios.get(API_URL)
-        .then(res => setSongs(res.data))
-        .catch(err => console.log(err))
+      //const songsByAlbumResponse =  fetch(API_URL+'/songsByAlbum');
+      axios.get(API_URL+'/songsByAlbum')
+      .then(result=> {
+      setSongsByAlbum(result.data)})
+      .catch(err => console.log(err))
+
+      axios.get(API_URL+'/songsByTitle')
+      .then(result=> {
+       setSongsByTitle(result.data)})
+      .catch(err => console.log(err))
+
+      axios.get(API_URL+'/songsByArtist')
+      .then(result=> {
+       setSongsByArtist(result.data)})
+      .catch(err => console.log(err))
+
+      axios.get(API_URL+'/songsByGenere')
+      .then(result=> {
+       setSongsByGenere(result.data)})
+      .catch(err => console.log(err))
+ 
+      axios.get(API_URL)
+      .then(result => setSongs(result.data))
+      .catch(err => console.log(err))
+
+    }, [])
+
+  const handelDelete  = (id) =>
+  {
+    axios.delete(API_URL+'/deleteSong/'+id)
+    .then(res => {
+        console.log(res)
+        window.location.reload()
     })
+    .catch(err => console.log(err))
+  }
     return (
         <div className="d-flex vh-100 bg-secondary justify-content-center align-items-center">
-            <div className="w-50 bg-white rounded p-3">
-                <Link to="/create" className="btn btn-success">Add +</Link>
+            <div className="w-350 bg-white rounded p-5">
+                <Link to="/create" className="btn btn-success" style={{width: "150px" , marginRight: "350px"}}>Add +</Link>
                <table className="table">
                 <thead>
                     <tr>
-                        <th>Title</th>
-                        <th>Artist</th>
-                        <th>Album</th>  
-                        <th>Genre</th>
-                        <th>Action</th>
+                        <th>Title <br></br>#{songsByTitle} </th>
+                        <th>Artist <br></br>#{songsByArtist}</th>
+                        <th>Album <br></br>#{songsByAlbum}</th>  
+                        <th>Genre <br></br>#{songsByGenere}</th>
+                        <th>Action </th>
                     </tr>
                 </thead>
                 <tbody>
@@ -33,8 +71,12 @@ function Songs () {
                             <td>{song.artist}</td>
                             <td>{song.album}</td>
                             <td>{song.gener}</td>
-                            <td><Link to={`/update/${song._id}`} className="btn btn-success">Update</Link>
-                           <button className="btn btn-danger">Delete</button></td>
+                            <td><Link to={`/update/${song._id}`} className="btn btn-outline-primary p-2 ml-2">Update</Link> 
+                            </td>
+                            <td>
+                           <button className="btn btn-outline-danger p-2 ml-5"
+                           onClick={(e) => handelDelete(song._id)}>Delete</button></td>
+                        
                         </tr>
                     })
                    }
